d7a373f8b3d9464048a8dc76d8044c864dcc2384
calcishard <jasontran2134@gmail.com>
2025-02-22 13:55:01 -0500

    created endpoint for front end to access db

--- database.py
+++ database.py
@@ -1,15 +1,16 @@
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
+
+app = Flask(__name__)
 
 client = MongoClient("mongodb+srv://jasontran2134:y9kSSG40xcK1oyZp@cluster0.z2ry0.mongodb.net/?ssl=true")
 db = client["Cluster0"]
 collection = db["users"]
 
-def insert_or_update_user(username, uncompleted_goals, completed_goals, distance_scrolled, time_spent):
-    collection.update_one(
+def increment_data(username, distance_scrolled, time_spent):
+    result = collection.update_one(
         {"username": username},
         {
-            "$set": {"uncompleted_goals": uncompleted_goals,
-                     "completed_goals": completed_goals},  # Update goals if changed
             "$inc": {
                 "distance_scrolled": distance_scrolled,
                 "time_spent_on_social_media_today": time_spent
@@ -17,12 +18,24 @@ def insert_or_update_user(username, uncompleted_goals, completed_goals, distance
         },
         upsert=True  # If user doesn't exist, create a new one
     )
-    print(f"User {username} updated or inserted.")
+    print(f"User {username} updated")
+    return result
 
-# insert_or_update_user("jason", ["Read 10 pages", "Do 50 pushups"], 550, 35)
-user = collection.find_one({"username": "jason"})
-if user:
-    print(user)
-else:
-    print("User not found.")
+@app.route('/increment_user', methods=['POST'])
+def update_user():
+    data = request.get_json()
+    username = data.get('username')
+    distance_scrolled = data.get('distance_scrolled')
+    time_spent = data.get('time_spent')
 
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+    
+    result = increment_data(username, distance_scrolled, time_spent)
+
+    if result.matched_count == 0:
+        return jsonify({"error": "User not found"}), 404
+    return jsonify({"message": "sucessfully updated user"}), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
