5d97ca100af574df914d5f4db9dbd3a454c79f81
Md Rahbir Mahdi <rahbir1518@gmail.com>
2025-02-22 11:00:10 -0500

    TEST

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { Container, Stack, Text } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import CardGrid from "./components/CardGrid";
+
+function App() {
+	return (
+		<Stack minH={"100vh"}>
+			<Navbar/>
+
+			<Container maxW={"1200px"} my={4}>
+				<Text
+					fontSize={{ base: "3xl", md: "50" }}
+					fontWeight={"bold"}
+					letterSpacing={"2px"}
+					textTransform={"uppercase"}
+					textAlign={"center"}
+					mb={8}
+				>
+					<Text as={"span"} bgGradient={"linear(to-r, cyan.400, blue.500)"} bgClip={"text"}>
+						My Tasks
+					</Text>
+				</Text>
+
+				<CardGrid/>
+			</Container>
+		</Stack>
+	);
+}
+
+export default App;
